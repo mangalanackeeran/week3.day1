9c9e406cb74a85b5df48ce3a8e7b46300d77d410
mangalanackeeran <50294574+mangalanackeeran@users.noreply.github.com>
2022-03-10 22:21:33 +0530

    Assignment1

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package week3.day1;
+
+public class Desktop extends Computer{
+
+	public void desktopSize()
+	{
+		System.out.println("DesktopSize");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop d=new Desktop();
+		d.computerModel();
+		d.desktopSize();
+		
+	}
+
+}
